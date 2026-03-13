ad8f3d01bf3dafe56773c1131a2187afc07f2983
Sebastian <sebastian.debesa@hotmail.com>
2026-03-12 22:52:02 -0300

    feat: add UserRepository and UserService

---
+++ src/main/java/batial/fintrack/user/UserService.java
@@ -0,0 +1,22 @@
+package batial.fintrack.user;
+
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
